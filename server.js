113ae7e14f4d58c19bfcd138743053faa74397fe
Farid Rashidov <frashidov@gmail.com>
2024-06-15 17:01:30 +0400

    Change

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const mainRoute = require("./routes/index");
+const port = 3000;
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+// middlewares
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", mainRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on port ${port}`);
+});
